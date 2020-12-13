65575bbd8b66cd89a645cadde8f1bb61e27149f5
dev-ved30 <vedgs2@illinois.edu>
2020-12-13 05:29:07 -0500

    Added comments and cleaned up some code

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pdfminer.high_level import extract_text
 from tkinter import Tk, filedialog
 from gtts import gTTS
 import os
-import time
 
 START = 6
 END = 31
@@ -36,19 +35,41 @@ def input():
         return root.filename
 
 def PdfToText(path):
+    """
+    This function takes the path to a PDF from the START to END page numbers, 
+    converts it to a String, splits it along the splitter, and returns the list
+    of Strings.
+
+    Args:
+        path (String): [The path to the PDF file]
+
+    Returns:
+        [List]: [A list of Strings made up of the PDF text split across the SPLITTER]
+    """
     text = extract_text(path, page_numbers = range(START, END))
     arr = text.split(SPLITTER)
-    print(len(arr))
     return arr
 
 def TextToSpeech(text_arr):
+    """
+    Thie function takes an array of Strings and converts each String to an MP3 clip. It
+    also saves them locally.
+
+    Args:
+        text_arr ([List]): [A list of Strings that need to converted to MP3 clips.]
+    """
     language = 'en'
     i = 1
-    os.mkdir("results")
+    if not os.path.exists("results"):
+        os.mkdir("results")
     for text in text_arr:
         recording = gTTS(text=text, lang=language, slow=False) 
         recording.save("results/" + FILE + "_" + str(i) + ".mp3") 
+        print("Done with Chapter " + str(i))
         i = i + 1
+
+def UploadToRSS():
+    print("Test")
 
 path = input()
 text_arr = PdfToText(path)
