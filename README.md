58477f5378c3adb7f1afd9c4c2a7e02bb218ce31
dev-ved30 <vedgs2@illinois.edu>
2020-12-14 21:05:46 -0500

    Web automation now logs into anchor

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PdfCast
+`pip install pdfminer`
+`pip install gTTS`
+`pip install selenium`
+
+https://sites.google.com/a/chromium.org/chromedriver/downloads
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from pdfminer.high_level import extract_text
 from tkinter import Tk, filedialog
 from gtts import gTTS
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import os
+import time
 
-START = 6
+PATH = "WebDrivers/chromedriver"
+
+PODCAST_NAME = ""
+EMAIL = ""
+PASSWORD = ""
+
+START = 26
 END = 31
 SPLITTER = "< Chapter"
 
@@ -46,6 +55,7 @@ def PdfToText(path):
     Returns:
         [List]: [A list of Strings made up of the PDF text split across the SPLITTER]
     """
+
     text = extract_text(path, page_numbers = range(START, END))
     arr = text.split(SPLITTER)
     return arr
@@ -58,19 +68,54 @@ def TextToSpeech(text_arr):
     Args:
         text_arr ([List]): [A list of Strings that need to converted to MP3 clips.]
     """
+
     language = 'en'
     i = 1
-    if not os.path.exists("results"):
-        os.mkdir("results")
+    if not os.path.exists("resutls/" + FILE):
+        os.mkdir("results/" + FILE)
     for text in text_arr:
-        recording = gTTS(text=text, lang=language, slow=False) 
-        recording.save("results/" + FILE + "_" + str(i) + ".mp3") 
+        print(text)
+        recording = gTTS(text = text, lang = language, slow = False) 
+        recording.save("results/" + FILE + "/" + FILE + "_" + str(i) + ".mp3") 
         print("Done with Chapter " + str(i))
         i = i + 1
 
-def UploadToRSS():
-    print("Test")
+def UploadPodcast():
 
-path = input()
-text_arr = PdfToText(path)
-TextToSpeech(text_arr)
+
+    driver = webdriver.Chrome(PATH)
+
+    # Opening the website
+    driver.get("https://anchor.fm/dashboard/episode/new")
+
+    a = driver.page_source
+    # Entering Email
+    email = driver.find_element_by_id("email")
+    email.send_keys(EMAIL)
+
+    # Entering Password
+    password = driver.find_element_by_id("password")
+    password.send_keys(PASSWORD)
+
+    # Pressing Enter
+    password.send_keys(Keys.RETURN)
+
+    # Waiting for new code to be read by selenium
+    time.sleep(5)
+
+    # Clicking the button to open file dialog
+    uploader = driver.find_element_by_class_name("styles__uploadAudioIcon___1BcDm").click()
+    #keys = uploader.send_keys()
+
+    #uploader.send_keys(Keys.RETURN)
+
+    
+    time.sleep(100)
+    driver.quit()
+
+
+
+#path = input()
+#text_arr = PdfToText(path)
+#TextToSpeech(text_arr)
+UploadPodcast()
